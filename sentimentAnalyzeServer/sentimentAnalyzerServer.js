4fc6801d772c1f600048136309f69355ca34fa8d
sahremusa <48286632+sahremusa@users.noreply.github.com>
2021-03-15 17:15:47 +0100

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,49 @@
 const express = require('express');
-const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use(express.static('client'))
+function getNLUinstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingv1 = require('ibm-watson/natural-language-understanding/v1');
+    const {IamAuthenticator} = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingv1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+    })
+    serviceUrl: api_url,
+ }
+dotenv.use(express.static('client'))
 
 const cors_app = require('cors');
-app.use(cors_app());
+dotenv.use(cors_dotenv());
 
-app.get("/",(req,res)=>{
+dotenv.get("/",(req,res)=>{
     res.render('index.html');
   });
 
-app.get("/url/emotion", (req,res) => {
+dotenv.get("/url/emotion", (req,res) => {
 
     return res.send({"happy":"90","sad":"10"});
 });
 
-app.get("/url/sentiment", (req,res) => {
+dotenv.get("/url/sentiment", (req,res) => {
     return res.send("url sentiment for "+req.query.url);
 });
 
-app.get("/text/emotion", (req,res) => {
+dotenv.get("/text/emotion", (req,res) => {
     return res.send({"happy":"10","sad":"90"});
 });
 
-app.get("/text/sentiment", (req,res) => {
+dotenv.get("/text/sentiment", (req,res) => {
     return res.send("text sentiment for "+req.query.text);
 });
 
-let server = app.listen(8080, () => {
+let server = dotenv.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
